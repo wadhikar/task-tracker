30f595ae025fb11d4fefaaf6447ceb21d2c40005
Wilson Adhikari <wadhikar@users.noreply.github.com>
2021-04-22 23:34:15 -0700

    Add AddTask component for Add button form
    Format files with Prettier

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
       text: 'Make Lunch',
       day: 'Apr 22nd at 11:30am',
-      reminder: false
+      reminder: false,
     },
-  ])
+  ]);
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   const toggleReminder = (id) => {
     setTasks(
-      tasks.map((task) => 
+      tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
-    )
-  }
-  
+    );
+  };
+
+  const toggleShowAddForm = () => {
+    setShowAddTask(!showAddTask);
+  };
+
   return (
-    <div className="container">
-      <Header title='Task Tracker'/>
-      {
-        tasks.length > 0 ? (
+    <div className='container'>
+      <Header
+        title='Task Tracker'
+        onAdd={toggleShowAddForm}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-        ) : (
-          'No Tasks to show'
-        )
-      }
+      ) : (
+        'No Tasks to show'
+      )}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <button 
+    <button
       style={{ backgroundColor: color }}
       className='btn'
       onClick={onClick}
     >
       {text}
     </button>
-  )
-}
+  );
+};
 
 Button.defaultProps = {
-  color: 'steelblue'
-}
+  color: 'steelblue',
+};
 
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func,
-}
+};
 
-export default Button
+export default Button;
