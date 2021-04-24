649b8617368fd1bda0ea8101d75f2bedd9b5253e
Wilson Adhikari <wadhikar@users.noreply.github.com>
2021-04-23 20:10:54 -0700

    Build Django GraphQL server to create/query tasks

---
+++ backend/myproject/gql_server/schema.py
@@ -0,0 +1,48 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import TaskModel
+
+
+class TaskType(DjangoObjectType):
+  class Meta:
+    model = TaskModel
+
+
+class Query(graphene.ObjectType):
+  tasks = graphene.List(TaskType)
+
+  def resolve_tasks(self, info):
+    return TaskModel.objects.all()
+    
+
+class CreateTask(graphene.Mutation):
+  id = graphene.ID()
+  text = graphene.String()
+  day = graphene.Date()
+  reminder = graphene.Boolean()
+
+  class Arguments:
+    text = graphene.String()
+    day = graphene.Date() # YYYY-MM-DD
+    reminder = graphene.Boolean()
+
+  def mutate(self, info, text, day, reminder):
+    task = TaskModel(text=text, day=day, reminder=reminder)
+    task.save()
+
+    return CreateTask(
+      id = task.id,
+      text = task.text,
+      day = task.day,
+      reminder = task.reminder,
+    )
+
+
+class Mutation(graphene.ObjectType):
+  create_user = CreateTask.Field()
+
+schema = graphene.Schema(
+  query=Query,
+  mutation=Mutation
+)
