9364851f5b69ab27c09177d49a8f67fcdcbee26c
Wilson Adhikari <wadhikar@users.noreply.github.com>
2021-04-24 23:06:35 -0700

    Cleanup comments in Django Models file

--- backend/myproject/gql_server/models.py
+++ backend/myproject/gql_server/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-# {
-#     id: 1,
-#     text: 'Make Lunch',
-#     day: 'Apr 22nd at 11:30am',
-#     reminder: false,
-# }
-
-
 class TaskModel(models.Model):
     text = models.TextField()
     day = models.DateField()
