65251738bd41a66590cab5fdcc8849cf7b16edc7
Wilson Adhikari <wadhikar@users.noreply.github.com>
2021-04-24 23:03:02 -0700

    Add Update Mutation to toggle reminder for client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,6 @@ import AddTaskForm from './components/AddTaskForm';
 function App() {
   const [showAddTaskForm, setShowAddTaskForm] = useState(false);
 
-  // TODO: Reflect Reminder Toggle in GraphQL
-  // const toggleReminder = (id) => {
-  //   setTasks(
-  //     tasks.map((task) =>
-  //       task.id === id ? { ...task, reminder: !task.reminder } : task
-  //     )
-  //   );
-  // };
-
   const toggleShowAddForm = () => {
     setShowAddTaskForm(!showAddTaskForm);
   };
